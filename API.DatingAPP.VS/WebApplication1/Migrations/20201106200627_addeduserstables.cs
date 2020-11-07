244055c9726026f61c1d5f48bd0aaba0c32e662f
jahangeer mirza <jahangeer.mirza@hotmail.com>
2020-11-07 12:21:29 +0500

    initial commit

---
+++ API.DatingAPP.VS/WebApplication1/Migrations/20201106200627_addeduserstables.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class addeduserstables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "values",
+                newName: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true),
+                    PasswordHashed = table.Column<byte[]>(nullable: true),
+                    SaltHashed = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "values",
+                newName: "id");
+        }
+    }
+}
---
+++ API.DatingAPP.VS/WebApplication1/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHashed { get; set; }
+        public byte[] SaltHashed { get; set; }
+
+    }
+}
